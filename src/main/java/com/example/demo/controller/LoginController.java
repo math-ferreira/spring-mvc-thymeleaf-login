08eec402b17a0c6aa05b079dec504265a235e7ac
reidomath <mat.s.ferreira@gmail.com>
2020-01-04 20:54:11 -0300

    primeira versao

---
+++ src/main/java/com/example/demo/controller/LoginController.java
@@ -0,0 +1,30 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.example.demo.form.LoginForm;
+
+@Controller
+public class LoginController {
+	
+	@RequestMapping(value="/login", method = RequestMethod.GET)
+	public String getLoginForm() {
+		return "login";
+	}
+	
+	@RequestMapping(value="/login", method = RequestMethod.POST)
+	public String login(@ModelAttribute (name ="loginForm") LoginForm loginForm, Model model) {
+		String username = loginForm.getUsername();
+		String password = loginForm.getPassword();
+		
+		if ("admin".equals(username) && "admin".equals(password)) {
+			return "home";
+		}
+		model.addAttribute("invalidCredentials", true);
+		return "login";
+	}
+}
